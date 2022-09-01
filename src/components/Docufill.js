87f24dcba38eaf347bd76981f00d52f798fc5957
crispymangoes <crispymangoes@gmail.com>
2022-09-01 10:31:34 -0700

    refactor into two files

--- src/components/Docufill.js
+++ src/components/Docufill.js
@@ -1,76 +1,13 @@
 import React from "react";
 import { useFormik } from "formik";
-import Docxtemplater from 'docxtemplater';
-import PizZip from 'pizzip';
-import PizZipUtils from 'pizzip/utils/index.js';
-import { saveAs } from 'file-saver';
+import { generateDocument } from "./GenerateDocument";
 
 const formSchema = {
     first_name: "",
     last_name: "",
     phone: "",
-    description: ""
+    description: "Default"
 };
-
-function loadFile(url, callback) {
-    PizZipUtils.getBinaryContent(url, callback);
-  }
-
-const generateDocument = (form) => {
-    loadFile(
-      'https://docxtemplater.com/tag-example.docx',
-      function (error, content) {
-        if (error) {
-          throw error;
-        }
-        var zip = new PizZip(content);
-        var doc = new Docxtemplater(zip, {
-          paragraphLoop: true,
-          linebreaks: true,
-          //Think I can add a delimeters object here to specifiy the  delimiters
-        });
-        doc.setData(form);
-        try {
-          // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-          doc.render();
-        } catch (error) {
-          // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
-          function replaceErrors(key, value) {
-            if (value instanceof Error) {
-              return Object.getOwnPropertyNames(value).reduce(function (
-                error,
-                key
-              ) {
-                error[key] = value[key];
-                return error;
-              },
-              {});
-            }
-            return value;
-          }
-          console.log(JSON.stringify({ error: error }, replaceErrors));
-
-          if (error.properties && error.properties.errors instanceof Array) {
-            const errorMessages = error.properties.errors
-              .map(function (error) {
-                return error.properties.explanation;
-              })
-              .join('\n');
-            console.log('errorMessages', errorMessages);
-            // errorMessages is a humanly readable message looking like this :
-            // 'The tag beginning with "foobar" is unopened'
-          }
-          throw error;
-        }
-        var out = doc.getZip().generate({
-          type: 'blob',
-          mimeType:
-            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        }); //Output the document using Data-URI
-        saveAs(out, 'output.docx');
-      }
-    );
-  };
 
 export const Docufill = () => {
   const formik = useFormik({
